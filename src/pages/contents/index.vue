d4ac803c275cbf53c3ff147b953e174a8cbdce3b
abcmPrivate <MoekoEbisawa@gmail.com>
2020-11-22 23:01:38 +0900

    remove: duplicate methods

--- src/pages/contents/index.vue
+++ src/pages/contents/index.vue
@@ -2,12 +2,7 @@
   <div class="contents">
     <Frame>
       <template v-slot:main>
-        <div>
-          <form ref="form">
-            <input type="text" v-model="clapMessage" placeholder="hoge" />
-            <button type="button" @click="send">送る</button>
-          </form>
-        </div>
+        <p>main</p>
       </template>
       <template v-slot:sub>
         <nuxt-link :to="{ name: 'index' }">index</nuxt-link>
@@ -22,30 +17,6 @@ import firebase from '@/plugins/firebase';
 
 export default {
   layout: 'contents',
-  data() {
-    return {
-      clapMessage: '',
-      isSending: false,
-    };
-  },
-  methods: {
-    async send() {
-      if (!this.clapMessage) return;
-
-      this.isSending = true;
-      const functions = firebase.app().functions('asia-northeast1');
-
-      const mailer = functions.httpsCallable('sendMail');
-      await mailer({ message: this.clapMessage })
-        .then(() => {
-          console.log('send mail');
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-      this.isSending = false;
-    },
-  },
 };
 </script>
 
