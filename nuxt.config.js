ad6d09325df0ce9d2a3fb3b1ee969c86058d58c3
abcmPrivate <MoekoEbisawa@gmail.com>
2020-11-22 15:54:40 +0900

    fixed: scss調整

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,7 @@ export default {
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: ['modern-css-reset', '@/assets/scss/base/index.scss'],
+  css: ['modern-css-reset', { src: '~assets/scss/base/index.scss', lang: 'scss' }],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [],
@@ -26,6 +26,7 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
+    '@nuxtjs/style-resources',
   ],
 
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
@@ -33,4 +34,8 @@ export default {
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {},
+
+  styleResources: {
+    scss: ['./assets/scss/variables/index.scss'],
+  },
 };
