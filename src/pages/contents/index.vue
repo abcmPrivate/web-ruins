8487f6068ae69272a64a3aad9bca85557797178d
abcmPrivate <MoekoEbisawa@gmail.com>
2020-11-22 20:15:59 +0900

    add: send webclap

--- src/pages/contents/index.vue
+++ src/pages/contents/index.vue
@@ -3,7 +3,10 @@
     <Frame>
       <template v-slot:main>
         <div>
-          ほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげ
+          <form ref="form">
+            <input type="text" v-model="clapMessage" placeholder="hoge" />
+            <button type="button" @click="send">送る</button>
+          </form>
         </div>
       </template>
       <template v-slot:sub>さぶさぶ</template>
@@ -12,8 +15,31 @@
 </template>
 
 <script>
+import firebase from '@/plugins/firebase';
+
 export default {
   layout: 'contents',
+  data() {
+    return {
+      clapMessage: '',
+    };
+  },
+  methods: {
+    async send() {
+      if (!this.clapMessage) return;
+      const functions = firebase.app().functions('asia-northeast1');
+      console.log(functions);
+
+      const mailer = functions.httpsCallable('sendMail');
+      await mailer({ message: this.clapMessage })
+        .then(() => {
+          console.log('send mail');
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+  },
 };
 </script>
 
