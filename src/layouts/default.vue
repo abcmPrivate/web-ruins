5303dbfb66f691231ee7944d198094cde3109ebd
abcmPrivate <MoekoEbisawa@gmail.com>
2020-11-29 12:13:47 +0900

    modified: enter page

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,11 +1,19 @@
 <template>
   <div class="container">
     <Nuxt />
+    <div class="container-footer">
+      <p>powered by firebase</p>
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
 .container {
   @include layout-container;
+  &-footer {
+    margin-top: 40px;
+    font-size: 12px;
+    text-align: center;
+  }
 }
 </style>
